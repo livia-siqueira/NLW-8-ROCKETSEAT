379718775c5ee5f82e56f117c4e95c206918fabc
livia-siqueira <livia.siqueira12@outlook.com>
2022-06-07 19:36:17 -0300

    feat: add theme initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import { Widget } from "./components/Widget";
 import "./global.css";
 
 function App() {
+  useEffect(() => {
+    localStorage.setItem("Theme", "ligth");
+  }, []);
   return (
     <div className="w-screen h-screen  dark:bg-zinc-700">
       <Widget />
